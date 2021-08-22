742f1dafa1241062c0d6c98d2127155375087f76
Metilda Chee Heng Er <metilda.cheehe@shopee.com>
2021-08-22 23:25:45 +0800

    Add better finals

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 
 
 public class Duke {
-    private final static String GOOD_BYE = "bye";
+    private final static String END_PROGRAM = "bye";
+    private final static String END_STATEMENT = "Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        while (!input.toLowerCase(Locale.ROOT).equals(GOOD_BYE)) {
+        while (!input.toLowerCase(Locale.ROOT).equals(END_PROGRAM)) {
             System.out.println(input);
             input = in.nextLine();
         }
-        System.out.println(GOOD_BYE);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
