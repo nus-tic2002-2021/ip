80fcbc1e39c0575a223e4a68fc525b0fe561091f
Metilda Chee Heng Er <metilda.cheehe@shopee.com>
2021-08-22 23:24:45 +0800

    Finish echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Duke {
+    private final static String GOOD_BYE = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (!input.toLowerCase(Locale.ROOT).equals(GOOD_BYE)) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
+        System.out.println(GOOD_BYE);
     }
 }
