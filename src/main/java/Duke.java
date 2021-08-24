25a09cf7d1100723055251dc45deccd0e2177528
DESKTOP-26G0JMD\Soon Boon <taysb92@gmail.com>
2021-08-24 20:01:58 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke ");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        String line = "";
+
+        while(true) {
+
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equalsIgnoreCase("bye"))
+                break;
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
+
+
 }
