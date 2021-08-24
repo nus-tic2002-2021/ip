776438f96fe9a28f8aa243873f301c8c87aca60c
Alex Ooi <alexooi1990@gmail.com>
2021-08-24 12:18:49 +0800

    Level-2: Add task add and list

--- src/main/java/DukePrompt.java
+++ src/main/java/DukePrompt.java
@@ -1,22 +1,27 @@
+import java.util.ArrayList;
+
 public class DukePrompt {
     public enum Prompts {
         START,
         END,
+        ADD,
         MANUAL
     }
 
-    private static final String HORIZONTAL_LINE = "    _________________________________________";
-    private static final String START = "    Hello! I'm Duke\n    What can I do for you?\n";
-    private static final String END = "    Bye. Hope to see you again soon!\n";
+    private static final String PADDING = "    ";
+    private static final String HORIZONTAL_LINE = PADDING + "_________________________________________";
+    private static final String START = "Hello! I'm Duke\n    What can I do for you?";
+    private static final String END = "Bye. Hope to see you again soon!";
+    private static final String ADD = "added: ";
 
     public static String getPrompt(Prompts decision) {
-        return getPrompt(decision, null);
+        return getPrompt(decision, "");
     }
 
     public static String getPrompt(Prompts decision, String man) {
         String output = "";
 
-        output += HORIZONTAL_LINE + System.lineSeparator();
+        output += HORIZONTAL_LINE + System.lineSeparator() + PADDING;
 
         switch (decision) {
             case START:
@@ -25,13 +30,29 @@ public class DukePrompt {
             case END:
                 output += END;
                 break;
+            case ADD:
+                output += ADD + man;
+                break;
             default:
-                output += "    " + man + System.lineSeparator();
+                output += man;
                 break;
         }
 
-        output += HORIZONTAL_LINE;
+        output += System.lineSeparator() + HORIZONTAL_LINE;
 
         return output;
+    }
+
+    public static String getPrompt(Prompts decision, ArrayList<String> outputs) {
+        String formattedInput = "";
+
+        for (int i=0;i < outputs.size();i++) {
+            if (i > 0) {
+                formattedInput += System.lineSeparator() + PADDING;
+            }
+            formattedInput += (i + 1) + ". " + outputs.get(i);
+        }
+
+        return getPrompt(Prompts.MANUAL, formattedInput);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    private String description;
+
+    public Task() {
+
+    }
+
+    public Task(String description) {
+        setDescription(description);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
