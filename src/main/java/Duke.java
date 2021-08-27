8dbc0fe6795bd2a27636f03d195e669b1d6fc044
Shao Wei <tohswei123@gmail.com>
2021-08-27 21:31:39 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,24 +2,51 @@ import java.util.Scanner;
 public class Duke {
 
     private static String lineBreak = "\t________________________________________";
+    private static int inputCounter = 0;
+    private static String[] inputList = new String[100];
 
     public static void initialize (){
         Scanner in = new Scanner(System.in);
-        String inputLine = "Hello! I'm Duke\n\tWhat can I do for you?";
-        String[] inputList = new String[100];
+        String inputLine = "";
+
+        printIntro();
+
         while (!inputLine.equals("bye")) {
-            printResponse(inputLine);
             inputLine = in.nextLine();
+            printResponse(inputLine);
         }
-        printResponse("Bye. Hope to see you again soon!");
+    }
+    public static void printIntro(){
+        System.out.println(lineBreak);
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        System.out.println(lineBreak);
     }
     public static void printResponse(String inputMsg) {
         System.out.println(lineBreak);
-        System.out.println("\t" + inputMsg);
+        if (inputMsg.equals("bye")){
+            System.out.println("\tBye. Hope to see you again soon!");
+        }
+        else if(inputMsg.equals("list")){
+            printList();
+        }
+        else{
+            storeList(inputMsg);
+            System.out.println("\tadded: " + inputMsg);
+        }
         System.out.println(lineBreak);
     }
 
+    public static void storeList(String inputMsg) {
+        inputList[inputCounter]= inputMsg;
+        inputCounter = inputCounter + 1;
+    }
 
+    public static void printList() {
+        for(int i = 0; i < inputCounter; i++){
+            System.out.println("\t" + (i+1) + "." + inputList[i]);
+
+        }
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
