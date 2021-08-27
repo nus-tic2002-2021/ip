ad6e1e2b226e935f08a785e0330343cbaf082f44
Shao Wei <tohswei123@gmail.com>
2021-08-27 20:01:08 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+
+    private static String lineBreak = "\t________________________________________";
+
+    public static void initialize (){
+        Scanner in = new Scanner(System.in);
+        String inputLine = "Hello! I'm Duke\n\tWhat can I do for you?";
+        String[] inputList = new String[100];
+        while (!inputLine.equals("bye")) {
+            printResponse(inputLine);
+            inputLine = in.nextLine();
+        }
+        printResponse("Bye. Hope to see you again soon!");
+    }
+    public static void printResponse(String inputMsg) {
+        System.out.println(lineBreak);
+        System.out.println("\t" + inputMsg);
+        System.out.println(lineBreak);
+    }
+
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +28,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        initialize();
+
     }
 }
