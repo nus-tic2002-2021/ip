9d7a480f7dcf27cca682cad630b99fc3eb8baa4f
Lin Gang <lingang1994@gmail.com>
2021-08-28 19:53:48 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        boolean end = false;
+        String command;
+        Scanner in = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        command = in.nextLine();
+        while(!end){
+            if(command.equals("bye")){
+                end = true;
+            }
+            else{
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    "+command);
+                System.out.println("    ____________________________________________________________");
+                command = in.nextLine();
+            }
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
