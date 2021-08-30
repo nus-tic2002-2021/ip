8090993044bb2f77c008d632b865834b0499a2b5
Gu Bicheng <e0374027@u.nus.edu>
2021-08-30 19:51:00 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String text;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello I'm Alice\nWhat can i do for you?");
+        System.out.println("=======================================================");
+
+        text = in.nextLine();
+
+        while(text != null){
+            if (text.equals("bye")){
+                System.out.println("Bye, i will miss you.");
+                System.out.println("=========================END===========================");
+                return;
+            } else {
+                System.out.println(text);
+                System.out.println("=======================================================");
+                text = in.nextLine();
+            }
+        }
     }
+
+    /*  can i create a method called Echo rather than Echo inside main()?
+    public static void Echo(Scanner args){
+        while(text != null){
+            if (text.equals("bye")){
+                System.out.println("Bye, i will miss you.");
+                return;
+            } else {
+                System.out.println(text);
+                text = in.nextLine();
+            }
+        }
+    }
+    */
+
 }
