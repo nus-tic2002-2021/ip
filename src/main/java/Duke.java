b6aac5c3e003f004c6839f92589a3c4f89f81df7
xu xiang <xiangxu.troy@hotmail.com>
2021-08-31 16:30:20 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String line;
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            line = in.nextLine();
+            if (line.toLowerCase(Locale.forLanguageTag(line)).equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else {
+                System.out.println(line);
+            }
+        }
     }
+
 }
