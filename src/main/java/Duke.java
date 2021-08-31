43f50f9d549eac6292aaa4e444a2d1208193e23d
ksw95 <kshaowei95@gmail.com>
2021-08-31 21:54:04 +0800

    Added Echo function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void Echo() {
+        Boolean stop = false;
+        while (stop == false) {
+            String word = in.next();
+            if (word.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                stop = true;
+            } else {
+                System.out.println(word);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Echo();
     }
 }
