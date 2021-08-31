2d1a28be1e590d8ec37731a638d13cec81dbda71
noellimx <noellimx@gmail.com>
2021-09-01 00:59:50 +0800

    - setup test

---
+++ src/test/java/duke/coronet/level/Increment_00_Test.java
@@ -0,0 +1,34 @@
+
+
+package duke.coronet.level;
+
+import duke.coronet.testHelper.TestStream;
+import duke.coronet.manager.UxManager;
+import duke.coronet.orchestra.OrchestratorLevel00;
+import org.junit.Test;
+
+import static duke.coronet.testHelper.help.OutputUnderTest.getMsgUnderTestEntry;
+import static duke.coronet.testHelper.help.OutputUnderTest.getMsgUnderTestTerminate;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Implement an initial skeletal version of the Duke that simply greets the user and exits.
+ * <p>
+ * Uses OrchestratorLevel01
+ *
+ * @see <a href="https://nus-tic2002-2021.github.io/website/se-book-adapted/projectDuke/index.html#level-0-greet">Level-0</a>
+ */
+public class Increment_00_Test extends TestStream {
+    /**
+     * Orchestrator should display entry and exit message and terminates gently.
+     */
+    @Test
+    public void TestGreet() {
+        String expectedEntryMessage = getMsgUnderTestEntry();
+        String expectedExitMessage = getMsgUnderTestTerminate();
+        String expectedOutput = expectedEntryMessage + expectedExitMessage;
+        new OrchestratorLevel00(null, null, new UxManager(this.getPrintStream())).run();
+        assertEquals(expectedOutput, this.getOutput());
+    }
+
+}
---
+++ src/test/java/duke/coronet/testHelper/help/OutputUnderTest.java
@@ -0,0 +1,23 @@
+package duke.coronet.testHelper.help;
+
+public class OutputUnderTest {
+    private static String LOGO = " _                   _                                _             "
+            + System.lineSeparator()
+            + "| |                 | |                              | |                "
+            + System.lineSeparator()
+            + "| |_    __ _   ___  | | __  _ __ ___     __ _   ___  | |_    ___   _ __ "
+            + System.lineSeparator()
+            + "| __|  / _` | / __| | |/ / | '_ ` _ \\   / _` | / __| | __|  / _ \\ | '__|"
+            + System.lineSeparator()
+            + "| |_  | (_| | \\__ \\ |   <  | | | | | | | (_| | \\__ \\ | |_  |  __/ | |   "
+            + System.lineSeparator()
+            + " \\__|  \\__,_| |___/ |_|\\_\\ |_| |_| |_|  \\__,_| |___/  \\__|  \\___| |_|  "
+            + System.lineSeparator();
+
+    public static String getMsgUnderTestEntry() {
+        return "Hello from" + System.lineSeparator() + LOGO;
+    }
+    public static String getMsgUnderTestTerminate() {
+        return "See you again!" + System.lineSeparator();
+    }
+}
