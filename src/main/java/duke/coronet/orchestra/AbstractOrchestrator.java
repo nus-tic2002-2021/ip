922d92959e3328079b8d14562ae1041203775245
noellimx <noellimx@gmail.com>
2021-09-01 01:00:39 +0800

    - + orchestrator level 0

---
+++ src/main/java/duke/coronet/orchestra/AbstractOrchestrator.java
@@ -0,0 +1,42 @@
+package duke.coronet.orchestra;
+
+import duke.coronet.manager.FileResourceManager;
+import duke.coronet.manager.TaskManager;
+import duke.coronet.manager.UxManager;
+
+/**
+ * An orchestrator has member managers.
+ * Orchestrators inject dependency members to dependant member.
+ * A subclass object modifies behaviour to comply with assignment requirements
+ */
+public abstract class AbstractOrchestrator {
+    private TaskManager _taskManager;
+    private FileResourceManager _frMgr;
+    private UxManager _uxMgr;
+    public AbstractOrchestrator(TaskManager tm, FileResourceManager frm, UxManager uxMgr) {
+        this.setTaskManager(tm);
+        this.setFileResourceManager(frm);
+        this.setUxManager(uxMgr);
+    }
+    private AbstractOrchestrator() {
+    }
+    private void setTaskManager(TaskManager tm) {
+        this._taskManager = tm;
+    }
+    private void setFileResourceManager(FileResourceManager frm) {
+        this._frMgr = frm;
+    }
+    private void setUxManager(UxManager uxMgr) {
+        this._uxMgr = uxMgr;
+    }
+    private UxManager getUxManager() {
+        return this._uxMgr;
+    }
+    public void printEntryMessage() {
+        this.getUxManager().printEntryMessage();
+    }
+    public void printExitMessage() {
+        this.getUxManager().printTerminateMessage();
+    }
+    public abstract void run() throws Exception;
+}
