22420920785b0ad2e2c9c4f00b67177a2dce318b
ksw95 <kshaowei95@gmail.com>
2021-08-31 22:02:15 +0800

    Added StartDuke function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,15 @@ public class Duke {
 
     static Scanner in = new Scanner(System.in);
 
+    public static void StartDuke() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Welcome to\n" + logo + " chatbot! \n");
+    }
+
     public static void Echo() {
         Boolean stop = false;
         while (stop == false) {
@@ -18,12 +27,7 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        StartDuke();
         Echo();
     }
 }
