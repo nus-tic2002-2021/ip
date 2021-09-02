afc20bca35ba6ad060b6c0e1dd3217c228d9f2f2
L <xietian198701@gmail.com>
2021-09-02 14:02:34 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printLine(){
+        System.out.println("    --------------------------------");
+    }
+
+    public static void dukeGreet(){
+        printLine();
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?\n");
+        printLine();
+    }
+
+    public static void dukeEcho(){
+        String req;
+        Scanner sc = new Scanner(System.in);
+        while(!(req = sc.nextLine()).equals("bye")){
+            System.out.println(req + "\n");
+            printLine();
+            System.out.println("     " + req + "\n");
+            printLine();
+        }
+        System.out.println(req + "\n");
+        printLine();
+        System.out.println("     Bye.Hope to see you again soon!\n");
+        printLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +34,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        dukeGreet();
+        dukeEcho();
+
     }
 }
