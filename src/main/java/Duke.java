2b378e8333de92d294556789031f35b4fc0c7485
EndireKT <72508923+EndireKT@users.noreply.github.com>
2021-09-07 01:10:40 +0800

    Duke Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,10 +14,10 @@ public class Duke {
     public static void msgBye() throws IOException {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_________________________________");
-        buddahProtectMe();
+        msgBuddahProtectMe();
     }
 
-    public static void buddahProtectMe() throws IOException {
+    public static void msgBuddahProtectMe() throws IOException {
         String everything = "";
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/buddha.txt"))) {
             StringBuilder sb = new StringBuilder();
@@ -36,7 +36,7 @@ public class Duke {
     }
 
     public static void echo(String s) {
-        System.out.println("   " + s);
+        System.out.println("    added: " + s);
         System.out.println("_________________________________");
     }
 
@@ -44,14 +44,23 @@ public class Duke {
         msgGreet();
 
         boolean bye = false;
+
         String line;
         Scanner in = new Scanner(System.in);
 
+        List myList = new List();
+
         while (!bye) {
             line = in.nextLine();
-            if (line.equals("bye")) {
+            if (line.equals("list")) {
+                for (int i = 0; i < myList.getNumOfItem(); i++) {
+                    System.out.println("    " + Integer.toString(i) + ": " + myList.getItem(i));
+                }
+                System.out.println("_________________________________");
+            } else if (line.equals("bye")) {
                 bye = true;
             } else {
+                myList.addItem(line);
                 echo(line);
             }
         }
