60594732b2f19cf1c5bc24ac6a73301e9ed849de
ksw95 <kshaowei95@gmail.com>
2021-09-08 01:12:20 +0800

    Removed addTask and printChecklist functions from and added getDescription and getCompletedSymbol to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,29 +1,22 @@
 public class Task {
 
-    String Description;
-    boolean Completed;
-    static int TaskCount = 0;
-    static Task[] Checklist = new Task[100];
+    protected String Description;
+    protected boolean Completed;
 
     public Task(String newTask) {
         this.Description = newTask;
         this.Completed = false;
     }
 
-    public void addTask(String newTask) {
-        Task newEntry = new Task(newTask);
-        Checklist[TaskCount] = newEntry;
-        TaskCount++;
+    public String getDescription() {
+        return Description;
     }
 
-    public void printChecklist() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < TaskCount; i++) {
-            if (Checklist[i].Completed) {
-                System.out.println(i + ".[X] " + Checklist[i].Description);
-            } else {
-                System.out.println(i + ".[] " + Checklist[i].Description);
-            }
+    public String getCompletedSymbol() {
+        if (Completed) {
+            return "[X]";
+        } else {
+            return "[]";
         }
     }
 }
