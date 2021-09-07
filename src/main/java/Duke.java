212ec621464ac7266976ff4b3bcc82e294a16b08
DING MAN <55436197+DINGMAN17@users.noreply.github.com>
2021-09-07 12:20:56 +0800

    Level-2, comment out DUKE string

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,15 @@ public class Duke {
 
     public static void main(String[] args) {
         //print greeting messages
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
+
         String greeting = "Hey this is Blanc, welcome to your universe\n"
                 + "I will provide everything you need:))\n"
                 + "Please type any command to start:";
