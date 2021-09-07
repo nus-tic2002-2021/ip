8d1c41efc2c69d912f857686989e509a757aca05
DING MAN <55436197+DINGMAN17@users.noreply.github.com>
2021-09-07 11:37:41 +0800

    Level-1 done, added greeting and user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        //print greeting messages
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String greeting = "Hey this is Blanc, welcome to your universe\n"
+                + "I will provide everything you need:))\n"
+                + "Please type any command to start:";
+        System.out.println(greeting);
+        //take user input
+        Scanner in = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")){
+            command = in.nextLine();
+            System.out.println(command);
+        }
     }
 }
