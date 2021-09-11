7d0ee2f5678e9236b3d386ff03104227ae0978e1
Alex Ooi <alexooi1990@gmail.com>
2021-09-11 16:27:26 +0800

    Fix shellscript to also copy resources

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,6 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
+cp -R ../src/main/resources/ ../bin
 if ! find ../src/main/java -name "*.java" -print | xargs javac -cp ../src/main/java -Xlint:none -d ../bin
 then
     echo "********** BUILD FAILURE **********"
@@ -20,7 +21,7 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -cp ../bin com.alexooi.duke.Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
