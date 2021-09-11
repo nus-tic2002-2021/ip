d2d08bc865b1fee7a38490d41224aa6dde1b16df
Alex Ooi <alexooi1990@gmail.com>
2021-09-11 16:57:31 +0800

    Remove find command

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -33,8 +33,6 @@ else
     echo "dos2unix command not found. To prevent potential issues, please install dos2unix."
 fi
 
-find ../bin -type f
-
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
