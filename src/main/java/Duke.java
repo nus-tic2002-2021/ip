c2f462cc7d98538c9c41be8310c7b45dbdcf6d7b
iamMONSTER <jochuing83@gmail.com>
2021-09-14 22:04:56 +0800

    Cleanup console messages and code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void initProgram() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+    }
+
+    public static void terminateProgram() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0) ;
+    }
+
+    public static void main(String[] args) {
+
+        initProgram();
 
         String line = "";
         Scanner in = new Scanner(System.in).useDelimiter("\\n");
@@ -22,13 +32,12 @@ public class Duke {
             line = in.next();
 
             if(line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+                terminateProgram();
             } else if (line.equals("list")) {
                 tl.printTaskList();
                 isAddTask = false;
             } else if (line.contains("done")) {
-                System.out.println(line);
+                //System.out.println(line);
                 int index = Integer.parseInt(line.split(" ")[1]);
                 tl.setDone(index - 1);
                 isAddTask = false;
@@ -36,7 +45,7 @@ public class Duke {
 
             if(isAddTask) {
                 tl.addTask(line);
-                System.out.println(line);
+                //System.out.println(line);
             }
             isAddTask = true;
         }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -15,9 +15,12 @@ public class TaskList extends Task{
 
     public void setDone(int index) {
         this.taskList[index].setDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("[" + this.taskList[index].getStatusIcon() + "] " + this.taskList[index].getDescription());
     }
 
     public void printTaskList() {
+        System.out.println("Here are the tasks in your list: ");
         for (int i = 0; i < this.counter; i++) {
             Task temp = this.taskList[i];
             System.out.println(i+1 + ". [" + temp.getStatusIcon() + "] " + temp.getDescription());
