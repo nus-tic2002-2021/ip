a476e85a4539d33274a2fa61aa05d21dbb2e4efa
lea <leanneloves93@gmail.com>
2021-09-19 13:02:40 +0800

    add enumeration and update Duke.java code to use enmu, change the Task list to Arraylist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,12 @@
-import javax.xml.stream.events.DTD;
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static Task[] tasks = new Task[100];
+//     public static Task[] tasks = new Task[100];
+    public static ArrayList<Task> tasks = new ArrayList<Task>();
+
     private static int tasksCount = 0;
     //Declare the breakline as a class-level variable
     public static String breakLine = "____________________________________________________________\n";
@@ -19,124 +19,116 @@ public class Duke {
         + breakLine);
     }
 
-    public static void response(){
+    public static void getResponse(){
         Scanner in = new Scanner(System.in);
         String line = "";
-        String [] taskList = new String[2];
-
-        ArrayList<String> actionKeyChecker = new ArrayList<String>(); // initialise the array
-        actionKeyChecker.add("todo");
-        actionKeyChecker.add("deadline");
-        actionKeyChecker.add("event");
-        actionKeyChecker.add("done");
-
+        String taskStatus = "";
+        String taskDescription = "";
 
         //check user input
         while (true){
             line = in.nextLine();
-            String[] taskSplit = line.split(" ");
+            //remove the whitespace from end
+            line = line.stripTrailing();
+            String[] taskSplit = line.split(" ",2);    //taskSplit[0] -> setTaskStatus
 
-            String itemName = "";
-            taskList[0] = taskSplit[0];
-
-            try {
-                if (actionKeyChecker.contains(taskList[0])){
-                    itemName = line.substring(taskSplit[0].length()+1,line.length());
+            //check the input and make sure it's not bye
+            try{
+                taskStatus = taskSplit[0].toUpperCase();
+                if(line.contains("bye")){
+                    System.out.println(breakLine
+                                        + "Bye. Hope to see you again soon!\n" + breakLine);
+                    System.exit(0);
                 }
-                taskList[1] = itemName;
-                responseParse(taskList);
-            }catch (StringIndexOutOfBoundsException e){
-                System.out.println(breakLine
-                        +"☹ OOPS!!! The description of a "+taskList[0]+" cannot be empty.\n"
-                        +breakLine);
+
+                else if (line.contains("list")){
+                    printList();
+                }
+
+                else if(!contains(taskStatus)){
+                    throw new IllegalArgumentException("Invalid argument");
+                }
+
+                else {
+                    taskDescription = taskSplit[1].stripLeading();
+                    responseParse(taskStatus,taskDescription);
+                    //TODO should this task return the task then added to the list?
+                }
+
             }
-
-
-        }
-    }
-
-    public static void responseParse(String[] taskSplit){
-        String actionKey = taskSplit[0];
-        String actionValue = taskSplit[1];
-        HashMap textCollection = new HashMap();
-
-
-        Task t = new Task(actionValue);
-
-        if(actionKey.equals("list")){
-            printList();
-        }
-        else if(actionKey.equals("done")){
-//            String[] userKeyIn = t.description.split(" ");
-            //TODO write a class or function for done
-            //check the str behind the done is a digit
-            try
-            {
-                Integer.parseInt(actionValue);
-                int taskIndex = Integer.parseInt(actionValue)-1;
-                tasks[taskIndex].markAsDone();
-                printTaskDone(tasks[taskIndex]);
-            }
-            //check the input is integer
-            catch (NumberFormatException e)
-            {
-                System.out.println(taskSplit + " is not a valid integer");
-            }
-
-            //check the input is not 0, is not exceed the total tasks index
-            catch(NullPointerException e){
-                System.out.println("There are only "+tasksCount+" tasks, please enter the correct task number");
-            }
-            //check the 0 or negative
             catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("There are only "+tasksCount+" tasks, please enter the correct task number");
+                System.out.println(breakLine
+                                    +"☹ OOPS!!! The description of a "+taskStatus+" cannot be empty.\n"
+                                    +breakLine);
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(breakLine
+                        + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" + breakLine);
+            }
+        }
+    }
+
+    public static void responseParse(String taskStatus, String taskDescribe){
+        //parse the Status of task
+        Status status = Status.valueOf(taskStatus);
+        String describe = taskDescribe;
+        int taskIndex;
+        try{
+            switch (status){
+                case TODO:
+                    Todo todo = new Todo(describe);
+                    todo.setTaskStatus("T");
+                    tasks.add(todo);
+                    System.out.println(todo.toString());
+                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    break;
+                case DEADLINE:
+                    String[] deadlineAndTime = describe.split("/by");
+                    Deadline deadline = new Deadline(deadlineAndTime[0],deadlineAndTime[1]);
+                    deadline.setTaskStatus("D");
+                    tasks.add(deadline);
+                    System.out.println(deadline.toString());
+                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    break;
+                case EVENT:
+                    String[] eventAndTime = describe.split("/at");
+                    Event event = new Event(eventAndTime[0],eventAndTime[1]);
+                    event.setTaskStatus("E");
+                    tasks.add(event);
+                    System.out.println(event.toString());
+                    System.out.println("Now you have "+tasks.size()+" tasks in the list.");
+                    break;
+                case DELETE:
+                    taskIndex = Integer.parseInt(describe)-1;
+                    break;
+                case DONE:
+                    taskIndex = Integer.parseInt(describe)-1;
+                    tasks.get(taskIndex).markAsDone();
+                    printTaskDone(tasks.get(taskIndex));
+                    break;
             }
         }
 
-        else if(actionKey.equals("todo")){
-            tasks[tasksCount] = new Todo(actionValue);
-            tasks[tasksCount].setTaskStatus("T");
-            System.out.println(tasks[tasksCount].toString());
-            addTask(t);
-            System.out.println("Now you have "+tasksCount+" tasks in the list.");
-        }
-
-        else if(actionKey.equals("deadline")){
-            //get the index of /
-            String[] deadlineAndTime = actionValue.split("/by");
-            tasks[tasksCount] = new Deadline(deadlineAndTime[0],deadlineAndTime[1]);
-            tasks[tasksCount].setTaskStatus("D");
-
-            System.out.println(tasks[tasksCount].toString());
-            addTask(t);
-            System.out.println("Now you have "+tasksCount+" tasks in the list.");
-        }
-
-        else if(actionKey.equals("event")){
-            String[] eventAndTime = actionValue.split("/at");
-            tasks[tasksCount] = new Event(eventAndTime[0],eventAndTime[1]);
-            tasks[tasksCount].setTaskStatus("E");
-
-            System.out.println(tasks[tasksCount].toString());
-            addTask(t);
-            System.out.println("Now you have "+tasksCount+" tasks in the list.");
-
-        }
-        else if(actionKey.equals("bye")){
-            System.out.println(
-                breakLine + "Bye. Hope to see you again soon!\n" + breakLine);
-            System.exit(0);
-        }
-        else{
-            System.out.println(
-                    breakLine + "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" + breakLine);
-        }
-
+         //check the input is not 0, is not exceed the total tasks index
+         catch(NullPointerException e){
+             System.out.println("There are only "+tasksCount+" tasks, please enter the correct task number");
+         }
+         //check the 0 or negative
+         catch(ArrayIndexOutOfBoundsException e){
+             System.out.println("There are only "+tasksCount+" tasks, please enter the correct task number");
+         }
     }
 
-    public static void addTask(Task t){
-        tasksCount++;
+    public static boolean contains(String test) {
+
+        for (Status c : Status.values()) {
+            if (c.name().equals(test)) {
+                return true;
+            }
+        }
+        return false;
     }
+
 
     public static void printList(){
         String[] output = new String[10];
@@ -144,17 +136,18 @@ public class Duke {
         System.out.print(breakLine);
         System.out.println("Here are the tasks in your list:");
         //print out all items in list
-        for(int i = 0; i< tasksCount;i++){
-            output = tasks[i].toString().split("\n ");
+        // TODO change this part to ArrayList
+        for(int i = 0; i< tasks.size();i++){
+            output = tasks.get(i).toString().split("\n ");
             System.out.println(i+1 + "."+output[1]);
         }
         System.out.println(breakLine);
     }
 
     public static void updateList(String taskIsDone){
-        for(int i = 0; i< tasksCount;i++){
-            if (tasks[i].getDescription().contains(taskIsDone)) {
-                tasks[i].markAsDone();
+        for(int i = 0; i< tasks.size();i++){
+            if (tasks.get(i).getDescription().contains(taskIsDone)) {
+                tasks.get(i).markAsDone();
             }
         }
     }
@@ -167,6 +160,6 @@ public class Duke {
 
     public static void main(String[] args) {
         greeting();
-        response();
+        getResponse();
     }
 }
