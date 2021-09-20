86755209ec7cb3992dd2fb345ae3fd799a416a2d
Kwok Cheong <wongkwokcheong@hotmail.com>
2021-09-21 01:05:05 +0800

    added regex to listen for bye, added tasks class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
     public static ArrayList<String> storedResponses = new ArrayList<String>();
 
     public static void getList(){
         for (int i=1; i<storedResponses.size(); i++){
-            System.out.format("%d: " + storedResponses.get(i-1) + "\n" ,i);
+            System.out.format("%d: " + "[ ] " + storedResponses.get(i-1) + "\n" ,i);
         }
     }
 
@@ -31,11 +33,17 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String line = "";
+        String line;
         System.out.println("Hello! I'm Duke\nWhat can I do for you today?");
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
+            Pattern pattern = Pattern.compile("bye", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(line);
+            boolean matchFound = matcher.find();
+            if(matchFound) {
+              line = "bye";
+            }
             storedResponses.add(line);
             response(line);
         } while (!line.equals("bye"));
