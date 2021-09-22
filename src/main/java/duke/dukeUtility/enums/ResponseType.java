a77772a565fe4f97d60e1750855251e486a122f2
noellimx <noellimx@gmail.com>
2021-09-22 10:08:07 +0800

    - from ca4e3f672115dc64884656395279c372b9a04e52 as merge removed files

---
+++ src/main/java/duke/dukeUtility/enums/ResponseType.java
@@ -0,0 +1,13 @@
+package duke.dukeUtility.enums;
+
+/**
+ * @see duke.command.Command
+ */
+public enum ResponseType {
+    ECHO,
+    EXIT_LOOP,
+    ERROR_COMMAND_EXECUTION,
+    ERROR_REQUEST_INVALID_PARAMETERS,
+    TASK_CREATE_TODO,
+    TASK_LIST_ALL,
+}
