779912f165e28e81e2e1ba376734eede23c99a38
Maynard Wong <maynard.why@outlook.com>
2021-09-23 21:59:33 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Hello command
+        System.out.println("Hello, I'm Duke\n" + "What can I do for you?");
+        //read line
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("________________________________________________");
+        while(true){
+        line = in.nextLine();
+        System.out.println(line +"\n________________________________________________");
+            if(line.equals("bye")) {
+                System.out.println("Bye! Thanks for visiting The Duke!");
+                break;
+            }
+        }
     }
 }
