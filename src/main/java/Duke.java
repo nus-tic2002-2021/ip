dfff106ee9b932b645cc911f6788108575996e28
Huangfei Bai <>
2021-09-30 14:50:16 +0800

    level 1 rev

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,36 @@
 import java.util.Locale;
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String line;
+        System.out.println("Hello! I'm Duke\nHow can I help you today?");
+        String input;
         Scanner in = new Scanner(System.in);
+
+        String[] list = new String[100];
+        int listCount = 1;
+
+
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            line = in.nextLine();
-            if (line.toLowerCase(Locale.forLanguageTag(line)).equals("bye")) {
+            input = in.nextLine();
+            if (input.toLowerCase(Locale.forLanguageTag(input)).contains("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            else if (input.toLowerCase(Locale.forLanguageTag(input)).contains("list")){
+                for (int count = 1; count<listCount; count++) {
+                    if (count ==1){
+                    System.out.println(count + ". " +"[X]"+ list[count]);
+                    }else {
+                        System.out.println(count + ". " +"[ ]"+ list[count]);
+                    }
+                }
+            }
             else {
-                System.out.println(line);
+                System.out.println(input);
+                list[listCount] = input;
+                listCount++;
+                System.out.println("added: " + input);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setDone(boolean done){
+        this.isDone = done;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String print(){
+        String print = " [" + getStatusIcon()+"] "+getDescription();
+        return print;
+    }
+}
