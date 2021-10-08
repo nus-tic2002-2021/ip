4fac2ff7a4e62bea3133388f0730700cf6a02731
ksw95 <kshaowei95@gmail.com>
2021-10-09 01:55:56 +0800

    Added deadline class variables and constructor.

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,2 +1,8 @@
 public class Deadlines extends Task{
+    protected String deadline;
+
+    public Deadlines(String newTask, String datetime) {
+        super(newTask);
+        deadline = datetime;
+    }
 }
