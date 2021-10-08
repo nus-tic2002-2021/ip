a684de01044780b762f1fe4a59ccafb0e6f60527
WhasabiQ <88955634+WhasabiQ@users.noreply.github.com>
2021-10-08 23:53:36 +0800

    Level 2. Add, List
    Added 2 functions add_task and print_list. Mod printThis

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String lineBreak = "\n------------------- \n";
+    private static String[] task_list = new String[100];
+    private static int count = 0;
+    private static String lineBreak = "-------------------";
     public static void main(String[] args) {
         String logo = "  _       _ _      _ _______________________________________ \n"
                     + " | |  _  | | |    | |  __  |        |  __  |  __  |___   ___|\n"
@@ -17,13 +19,30 @@ public class Duke {
             in = new Scanner(System.in);
             line = in.nextLine();
             if(line.equalsIgnoreCase("bye")) {
-                printThis("Bye. Hope to see you again soon!");
+                printThis("Bye. Hope to see you again soon!",true);
                 break;
+            }else if(line.equalsIgnoreCase("list")){
+                print_list();
             }
-            printThis(line);
+            else{
+                add_task(line);
+            }
         }
     }
-    public static void printThis(String st){
-        System.out.println(lineBreak +st+lineBreak);
+    public static void printThis(String st, Boolean withLB){
+        if(withLB) System.out.println(lineBreak+"\n" +st+"\n"+lineBreak);
+        else System.out.println(st);
+    }
+    public static void add_task(String task){
+        task_list[count] = task;
+        count++;
+        printThis("added: " + task , true);
+    }
+    public static void print_list(){
+        System.out.println(lineBreak);
+        for(int i = 0; i < count; i++){
+           printThis(Integer.toString(i+1) + ". " + task_list[i],false);
+        }
+        System.out.println(lineBreak);
     }
 }
