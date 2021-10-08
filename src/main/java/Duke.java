cbf3087ef5562f64775a98725e0ae1a46b564dad
WhasabiQ <88955634+WhasabiQ@users.noreply.github.com>
2021-10-08 23:39:57 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String lineBreak = "\n------------------- \n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  _       _ _      _ _______________________________________ \n"
+                    + " | |  _  | | |    | |  __  |        |  __  |  __  |___   ___|\n"
+                    + " | | | | | | |----| | |__| |  ------| |__| | |__|/    | |    \n"
+                    + " | |_| |_| | |----| | |  | |------  | |  | | |__|\\ ___| |___\n"
+                    + " |_________|_|    |_|_|  |_|________|_|  |_|______|_________|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can do for you?");
+
+        Scanner in;
+        String line;
+        while(true){
+            in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equalsIgnoreCase("bye")) {
+                printThis("Bye. Hope to see you again soon!");
+                break;
+            }
+            printThis(line);
+        }
+    }
+    public static void printThis(String st){
+        System.out.println(lineBreak +st+lineBreak);
     }
 }
