f11609fe9ac3cebcfc9dbf2e8ea8b778ee707bbe
noellimx <noellimx@gmail.com>
2021-10-09 18:54:54 +0800

    - fix test with linux environment.

--- .github/workflows/run-deployment-test.sh
+++ .github/workflows/run-deployment-test.sh
@@ -25,11 +25,10 @@ mvn test
 
 # package into jar
 mvn package
-
 # test jar output
-java -jar target/Duke.jar < ./src/test/resources/test-input/input.txt > ./src/test/resources/test-output/actual.txt
+java -jar target/Duke.jar < ./src/test/resources/linux-test/test-input/input.txt > ./src/test/resources/linux-test/test-output/actual.txt
 
-cmp --print-chars ./src/test/resources/test-output/actual.txt ./src/test/resources/test-output/expected.txt
+cmp --print-chars ./src/test/resources/linux-test/test-output/actual.txt ./src/test/resources/linux-test/test-output/expected.txt
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
