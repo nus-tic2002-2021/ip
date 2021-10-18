eac94a82d3eb2a180722ef25fdd0c3a064f980c4
ksw95 <kshaowei95@gmail.com>
2021-10-19 00:01:16 +0800

    Created DukeException class with variables and methods.

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,15 @@
+public class DukeException extends Exception{
+    protected String ErrMsg;
+
+    public DukeException() {
+        this.ErrMsg = "";
+    }
+
+    public DukeException(String invalid) {
+        this.ErrMsg = invalid;
+    }
+
+    public void printErrMsg() {
+        System.out.println(ErrMsg);
+    }
+}
