9671688e109ac1e9f60e444b8a92b11f5cde55d5
ksw95 <kshaowei95@gmail.com>
2021-10-18 22:20:33 +0800

    Deleted now unused methods in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,6 @@ public class Duke {
 
     static Scanner in = new Scanner(System.in);
     static String line = "____________________________________________________________\n";
-    static String[] list = new String[100];
-    static int listCount = 0;
     static Task[] Checklist = new Task[100];
     static int TaskCount = 0;
 
@@ -23,70 +21,6 @@ public class Duke {
         String start = "Hello, I'm Duke\n"
                 + "What can I do for you?\n";
         System.out.println(line + start + line);
-    }
-
-    public static void Echo() {
-        Boolean stop = false;
-        while (stop == false) {
-            String word = in.nextLine();
-            if (word.equals("bye")) {
-                System.out.println(line + "Bye. Hope to see you again!\n" + line);
-                stop = true;
-            } else {
-                System.out.println(line + word + "\n" + line);
-            }
-        }
-    }
-
-    public static void AddDisplayList() {
-        Boolean stop = false;
-        while (stop == false) {
-            String input = in.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
-                listCount = 0;
-                stop = true;
-            } else if (input.equals("list")){
-                PrintList();
-            } else {
-                list[listCount] = input;
-                listCount++;
-                System.out.println(line + "added: " + input + "\n" + line);
-            }
-        }
-    }
-
-    public static void PrintList() {
-        System.out.println(line);
-        for (int i = 0; i < listCount; i++) {
-            System.out.println(i+1 + ". " + list[i]);
-        }
-        System.out.println(line);
-    }
-
-    public static void TaskList() {
-        Boolean stop = false;
-        while (stop == false) {
-            String input = in.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
-                listCount = 0;
-                stop = true;
-            } else if (input.equals("list")){
-                PrintChecklist();
-            } else if ((input.length() > 3) && (input.substring(0,4)).equals("done") ) {
-                if (input.length() < 5) {
-                    System.out.println(line + "\nInput missing the index of the task that is done.\n"
-                            + line);
-                } else {
-                    MarkIndex(input);
-                }
-            } else {
-                Task newTask = new Task(input);
-                AddTask(newTask);
-                System.out.println(line + "added: " + input + "\n" + line);
-            }
-        }
     }
 
     public static void AddTask(Task newEntry) {
@@ -197,31 +131,13 @@ public class Duke {
         System.out.println("Now you have " + TaskCount + " tasks in the list.");
     }
 
-    public static void Level1() {
-        Greet();
-        Echo();
-    }
-
-    public static void Level2() {
-        Greet();
-        AddDisplayList();
-    }
-
-    public static void Level3() {
-        Greet();
-        TaskList();
-    }
-
     public static void Level4() {
-        Greet();
         ExtendTaskList();
     }
 
     public static void main(String[] args) {
         StartDuke();
-        // Level1();
-        // Level2();
-        // Level3();
+        Greet();
         Level4();
     }
 }
