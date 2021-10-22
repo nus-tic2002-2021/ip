1087ec68db9b622bca89e04c048db565b07161cc
noellimx <noellimx@gmail.com>
2021-10-23 02:11:14 +0800

    checkstyle Compliance - CommandExportTasksToFile: imports ordering, javadocs

--- src/main/java/duke/command/systemcommand/CommandExportTasksToFile.java
+++ src/main/java/duke/command/systemcommand/CommandExportTasksToFile.java
@@ -1,18 +1,24 @@
 package duke.command.systemcommand;
 
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.stream.JsonWriter;
+
 import duke.command.Command;
 import duke.dukeUtility.enums.ResponseType;
 
-import java.util.List;
-
 public class CommandExportTasksToFile extends Command {
-    public CommandExportTasksToFile(JsonArray tasksJson, JsonWriter jw) {
+    /**
+     * Export tasks to file as JSON. Will return response type based on the execution.
+     * @param tasks task objects to write
+     * @param jw writer
+     */
+    public CommandExportTasksToFile(JsonArray tasks, JsonWriter jw) {
         super(ResponseType.EXPORT_IN_PROGRESS, List.of("export", jw.toString()));
         try {
-            new Gson().toJson(tasksJson, jw);
+            new Gson().toJson(tasks, jw);
             jw.close();
             this.setResponseType(ResponseType.FILE_SAVED);
         } catch (Exception error) {
