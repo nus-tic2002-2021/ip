57bc27db7ab031f3e60897ea3a2c6d6faa44800b
noellimx <noellimx@gmail.com>
2021-10-23 01:57:24 +0800

    checkstyle Compliance - CommandAddNewDeadline - imports ordering, line length, docs

--- src/main/java/duke/command/taskCommand/taskAdd/CommandAddNewDeadline.java
+++ src/main/java/duke/command/taskCommand/taskAdd/CommandAddNewDeadline.java
@@ -8,7 +8,14 @@ import duke.command.Command;
 import duke.dukeUtility.enums.ResponseType;
 
 public class CommandAddNewDeadline extends Command {
-    public CommandAddNewDeadline(TaskManager taskMgr, String taskDescription, LocalDateTime deadlineString) {
-        super(ResponseType.TASK_CREATE_DEADLINE, List.of("create", taskDescription, taskMgr.addNewDeadline(taskDescription, deadlineString).getTaskDescription()));
+    /**
+     * Task manager will add a new deadline to its collection with provided parameters
+     * @param tm task manager
+     * @param desc of deadline
+     * @param deadline of deadline
+     */
+    public CommandAddNewDeadline(TaskManager tm, String desc, LocalDateTime dl) {
+        super(ResponseType.TASK_CREATE_DEADLINE,
+            List.of("create", desc, tm.addNewDeadline(desc, dl).getTaskDescription()));
     }
 }
