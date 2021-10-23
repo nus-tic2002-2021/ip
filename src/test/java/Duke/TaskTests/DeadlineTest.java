2a5ae14b0790f9dac9d98ecb017bea3967bab132
Lin Gang <lingang1994@gmail.com>
2021-10-23 18:16:04 +0800

    Added tags for Gradle Support[Recommended] Added tags for JUnit Testing

--- src/test/java/Duke/TaskTests/DeadlineTest.java
+++ src/test/java/Duke/TaskTests/DeadlineTest.java
@@ -15,5 +15,7 @@ public class DeadlineTest {
         Deadline deadline = new Deadline("return book", sample);
         assertEquals("[D][ ] return book(by: 2021-12-25[SATURDAY] 16:00)", deadline.toString());
         assertEquals("D | 0 | return book | 2021-12-25 | 16:00", deadline.encodeTask());
+
+
     }
 }
