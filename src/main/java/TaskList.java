4ec073066f141abce1c688e7082f1f40054dba43
ksw95 <kshaowei95@gmail.com>
2021-10-29 02:52:31 +0800

    Created TaskList.java and moved TaskList methods from Duke.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    static ArrayList<Task> DukeList = new ArrayList<>();
+
+    public static void addTaskToList(Task newEntry) {
+        DukeList.add(newEntry);
+    }
+
+    public static void RemoveTask(int index) {
+        DukeList.remove(index);
+    }
+
+    public static void printTaskList() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < DukeList.size(); i++) {
+            System.out.println((i + 1) + "." + DukeList.get(i).getTaskInfo());
+        }
+    }
+
+    public static void MarkDone(String input) {
+        try {
+            if (Parser.CheckValidDone(input)) {
+                markTaskAtIndex(input);
+            }
+        } catch (DukeException e) {
+            e.printErrMsg();
+        }
+    }
+
+    public static void markTaskAtIndex(String input) throws DukeException {
+        int index = Integer.parseInt(input.substring(4).trim()) - 1;
+        if (index < DukeList.size() && index > -1) {
+            MarkTask(DukeList.get(index));
+            System.out.println("Nice! I've marked this task as done:\n  "
+                    + DukeList.get(index).getTaskInfo());
+        } else {
+            throw new DukeException("☹ OOPS!!! " +
+                    "The index number of the task to be done is invalid!");
+        }
+    }
+
+    public static void MarkTask(Task inputTask) {
+        inputTask.markCompleted();
+    }
+
+}
