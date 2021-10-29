a568db6ad1cc077b9305e707cf263340c34798d8
ksw95 <kshaowei95@gmail.com>
2021-10-29 10:53:41 +0800

    Created FileLineChecker class and moved all the file line checking methods from Duke.java to FileLineChecker.java

---
+++ src/main/java/FileLineChecker.java
@@ -0,0 +1,34 @@
+public class FileLineChecker {
+    public static boolean CheckTodoLine(String[] parts) {
+        if (parts.length != 3) {
+            return false;
+        } else if (parts[2].trim().equals("")) {
+            return false;
+        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean CheckDeadlineLine(String[] parts) {
+        if (parts.length != 4) {
+            return false;
+        } else if (parts[2].trim().equals("") || parts[3].trim().equals("")) {
+            return false;
+        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean CheckEventLine(String[] parts) {
+        if (parts.length != 4) {
+            return false;
+        } else if (parts[2].trim().equals("") || parts[3].trim().equals("")) {
+            return false;
+        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
+            return false;
+        }
+        return true;
+    }
+}
