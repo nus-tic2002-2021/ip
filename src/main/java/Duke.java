d2098100fb935404067cb93a002c0d012954065b
ksw95 <kshaowei95@gmail.com>
2021-10-29 10:53:50 +0800

    Refactored Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -23,189 +21,6 @@ public class Duke {
         }
     }
 
-    public static void AddTodo(String input) {
-        try {
-            if (Parser.CheckValidTodo(input)) {
-                String newTask = input.substring(4).trim();
-                Todo newTodo = new Todo(newTask);
-                TaskList.addTaskToList(newTodo);
-                Ui.PrintTaskAdded(newTodo);
-                Ui.PrintTaskCount();
-            }
-        } catch (DukeException e) {
-            e.printErrMsg();
-        }
-    }
-
-    public static void AddDeadline(String input) {
-        try {
-            if (Parser.CheckValidDeadline(input)) {
-                String[] parts = input.substring(8).split("/by");
-                Deadline newDeadline = new Deadline(parts[0].trim(), parts[1].trim());
-                TaskList.addTaskToList(newDeadline);
-                Ui.PrintTaskAdded(newDeadline);
-                Ui.PrintTaskCount();
-            }
-        } catch (DukeException e) {
-            e.printErrMsg();
-        }
-    }
-
-    public static void AddEvent(String input) {
-        try {
-            if (Parser.CheckValidEvent(input)) {
-                String[] parts = input.substring(5).split("/at");
-                Event newEvent = new Event(parts[0].trim(), parts[1].trim());
-                TaskList.addTaskToList(newEvent);
-                Ui.PrintTaskAdded(newEvent);
-                Ui.PrintTaskCount();
-            }
-        } catch (DukeException e) {
-            e.printErrMsg();
-        }
-    }
-
-    public static void DeleteTask(String input) {
-        try {
-            if (Parser.CheckValidDelete(input)) {
-                DeleteIndex(input);
-            }
-        } catch (DukeException e) {
-            e.printErrMsg();
-        }
-    }
-
-    public static void DeleteIndex(String input) throws DukeException {
-        int index = Integer.parseInt(input.substring(6).trim()) - 1;
-        if (index < TaskList.DukeList.size() && index > -1) {
-            String DeletedInfo = TaskList.DukeList.get(index).getTaskInfo();
-            TaskList.RemoveTask(index);
-            System.out.println("Noted! I've removed this task:\n  " + DeletedInfo);
-            Ui.PrintTaskCount();
-        } else {
-            throw new DukeException("☹ OOPS!!! The index number of the task to delete is invalid!");
-        }
-    }
-
-    public static Task ParseStorageLine(String FileLine) throws DukeException {
-        String[] parts = FileLine.split("\\|");
-        if (parts[0].trim().equals("T")) {
-            if(CheckTodoLine(parts)) {
-                Todo newTodo = getTodoFromLine(parts);
-                return newTodo;
-            }
-        } else if (parts[0].trim().equals("D")) {
-            if(CheckDeadlineLine(parts)) {
-                Deadline newDeadline = getDeadlineFromLine(parts);
-                return newDeadline;
-            }
-        } else if (parts[0].trim().equals("E")) {
-            if (CheckEventLine(parts)) {
-                Event newEvent = getEventFromLine(parts);
-                return newEvent;
-            }
-        } else {
-            System.out.println("Line is invalid");
-        }
-        throw new DukeException("A line from storage file is invalid and will not be added to Duke.");
-    }
-
-    public static Todo getTodoFromLine(String[] parts) {
-        Todo newTodo = new Todo(parts[2].trim());
-        if (parts[1].trim().equals("1")) {
-            TaskList.MarkTask(newTodo);
-        }
-        return newTodo;
-    }
-
-    public static Deadline getDeadlineFromLine(String[] parts) {
-        Deadline newDeadline = new Deadline(parts[2].trim(), parts[3].trim());
-        if (parts[1].trim().equals("1")) {
-            TaskList.MarkTask(newDeadline);
-        }
-        return newDeadline;
-    }
-
-    public static Event getEventFromLine(String[] parts) {
-        Event newEvent = new Event(parts[2].trim(), parts[3].trim());
-        if (parts[1].trim().equals("1")) {
-            TaskList.MarkTask(newEvent);
-        }
-        return newEvent;
-    }
-
-    public static boolean CheckTodoLine(String[] parts) {
-        if (parts.length != 3) {
-            return false;
-        } else if (parts[2].trim().equals("")) {
-            return false;
-        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean CheckDeadlineLine(String[] parts) {
-        if (parts.length != 4) {
-            return false;
-        } else if (parts[2].trim().equals("") || parts[3].trim().equals("")) {
-            return false;
-        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean CheckEventLine(String[] parts) {
-        if (parts.length != 4) {
-            return false;
-        } else if (parts[2].trim().equals("") || parts[3].trim().equals("")) {
-            return false;
-        } else if (!parts[1].trim().equals("1") && !parts[1].trim().equals("0")) {
-            return false;
-        }
-        return true;
-    }
-
-    public static void writeListToFile(File FileWrite) {
-        try {
-            FileWriter fw = new FileWriter(FileWrite, false);
-            ArrayList<Task> list = TaskList.DukeList;
-            for (int i = 0; i < list.size(); i++) {
-                Task taskAtIndex = list.get(i);
-                String typeCheck = taskAtIndex.getTaskType();
-                String newLine = "";
-                if (typeCheck.equals("T")) {
-                    newLine = buildStorageLine(taskAtIndex);
-                } else if (typeCheck.equals("D")) {
-                    newLine = buildStorageLine(taskAtIndex);
-                } else if (typeCheck.equals("E")) {
-                    newLine = buildStorageLine(taskAtIndex);
-                }
-                fw.write(newLine + System.getProperty("line.separator"));
-            }
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Write to file failed");
-        }
-    }
-
-    public static String buildStorageLine(Task targetTask) {
-        String divider = " | ";
-        String initial = targetTask.getTaskType();
-        String newLine = initial + divider;
-        if (targetTask.Completed) {
-            newLine = newLine + "1" + divider;
-        } else {
-            newLine = newLine + "0" + divider;
-        }
-        newLine = newLine + targetTask.Description;
-
-        if (initial.equals("D") || initial.equals("E"))
-        newLine = newLine + divider + targetTask.getAdditionalInfo();
-        return newLine;
-    }
-
     public static void RunDuke() {
         try {
             File StorageFile = Storage.OpenStorageFile();
@@ -221,7 +36,7 @@ public class Duke {
         RunDuke();
         ExtendTaskList();
         try {
-            writeListToFile(Storage.OpenStorageFile());
+            Storage.writeListToFile(Storage.OpenStorageFile());
         } catch (DukeException e) {
             System.out.println("Failed to write to file");
         }
