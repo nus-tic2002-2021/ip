aaf97a58d39f03a9bdedcb45476ae43ebc1fb7dd
DESKTOP-4D7T3LL\User <fatelan01@gmail.com>
2021-11-02 22:46:40 +0800

    Update UI for user instructions and date format

--- src/main/java/duke/UI.java
+++ src/main/java/duke/UI.java
@@ -10,23 +10,26 @@ public class UI {
     private final String DEADLINE_LENGTH_ERROR = "Deadline command has too many /by.";
     private final String EVENT_DESCRIPTION_ERROR = "Event command is missing a description and/or time.";
     private final String EVENT_LENGTH_ERROR = "Event command has too many /at.";
-    private final String INVALID_DATE_FORMAT = "Incorrect Date formatting.";
+    private final String INVALID_DATE_FORMAT = "Incorrect Date formatting. Only accepts yyyy-MM-dd HHmm format " +
+            "\n\te.g. 2021-10-21 1300";
 
     public void printIntro(){
         printLine();
         System.out.println("\tHello!");
         printInstruction();
-        System.out.println("\tWelcome. Please enter your instruction.");
+        System.out.println("\tWelcome to Toh Shao Wei TIC2002 Project. Please enter your instruction.");
         printLine();
     }
     public void printInstruction(){
         System.out.println("\t1.) Enter *todo 'text'*--- to add todo task");    //
-        System.out.println("\t2.) Enter *deadline 'text' /by 'text'*to add deadline task");
-        System.out.println("\t3.) Enter *event 'text' /at 'text'* to add event task");
+        System.out.println("\t2.) Enter *deadline 'text' /by 'DateTime'*to add deadline task");
+        System.out.println("\t3.) Enter *event 'text' /at 'DateTime'* to add event task");
         System.out.println("\t4.) Enter *list* to show list of tasks.");
         System.out.println("\t5.) Enter *done 'integer'* mark task as done.");
         System.out.println("\t6.) Enter *delete 'integer'* to delete task.");
-        System.out.println("\t7.) Enter *bye* to exit program");
+        System.out.println("\t7.) Enter *find 'text'* to search for the word.");
+        System.out.println("\t8.) Enter *view 'date'* to search all task on specific date");
+        System.out.println("\t9.) Enter *bye* to exit program");
     }
     public String readCommand(){
         Scanner in = new Scanner(System.in);
