faf62b322b4f2af7cb455c76b5db339ddc050730
Metilda Chee <metilda.cheehe@shopee.com>
2021-11-04 11:36:25 +0800

    Add parser and date, time in Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,41 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 public class Event extends Todo {
-    String at;
+    LocalDate at;
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public Event(String description, String start, int id) {
         super(description, id);
-        at = start;
+        setAt(start);
     }
 
-    public String getAt() {
+    public LocalDate parseStringToDate(String date) {
+        LocalDate parsedDate = null;
+        try {
+            parsedDate = LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            logger.info("parse date time got error" + e.getMessage());
+        }
+        return parsedDate;
+    }
+
+    public LocalDate getAt() {
         return at;
     }
 
     public void setAt(String start) {
-        at = start;
+        if (Objects.equals(start, "")) {
+            return;
+        }
+        LocalDate date = parseStringToDate(start);
+        if (date == null) {
+            logger.info("parsing date got null");
+        } else {
+            at = date;
+        }
     }
 
     @Override
