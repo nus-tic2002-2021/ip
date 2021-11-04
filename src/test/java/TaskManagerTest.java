d71918f7b316f34653cdc480f014405f482413e6
Metilda Chee <metilda.cheehe@shopee.com>
2021-11-04 09:07:03 +0800

    Add Gradle support and add create test cases

---
+++ src/test/java/TaskManagerTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskManagerTest {
+
+    @Test
+    public void createTodo() {
+        String eventType = "todo";
+        String eventInfo = "read books";
+
+        TaskManager tm = new TaskManager();
+        Task t = tm.createTask(eventInfo, eventType);
+        Todo e = (Todo) t;
+        assertEquals(e.getType(), eventType);
+        assertEquals(e.getDescription(), eventInfo);
+    }
+
+    @Test
+    public void createEventTest() {
+        String eventType = "event";
+        String eventDesc = "project meeting";
+        String eventTime = "Mon 2-4pm";
+        String eventInfo = eventDesc + "/at" + eventTime;
+
+        TaskManager tm = new TaskManager();
+        Task t = tm.createTask(eventInfo, eventType);
+        assertEquals(t.getType(), eventType);
+        Event e = (Event) t;
+        assertEquals(e.getDescription(), eventDesc);
+        assertEquals(e.getAt(), eventTime);
+    }
+}
