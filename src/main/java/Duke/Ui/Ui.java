c58d1fdcaf9dc0b76008f7892b7c2b55d891819f
ksw95 <kshaowei95@gmail.com>
2021-11-07 01:08:57 +0800

    Added comments to the class and methods in Ui.java

--- src/main/java/Duke/Ui/Ui.java
+++ src/main/java/Duke/Ui/Ui.java
@@ -3,9 +3,15 @@ package Duke.Ui;
 import Duke.Models.Task;
 import Duke.TaskList;
 
+/**
+ * Ui class holds Ui display methods that interact with the user.
+ */
 public class Ui {
     public static String line = "____________________________________________________________";
 
+    /**
+     * Prints the opening message of Duke.
+     */
     public static void StartDuke() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,16 +21,26 @@ public class Ui {
         System.out.println(line + "\nWelcome to\n" + logo + " chatbot!");
     }
 
+    /**
+     * Prints a greeting from Duke.
+     */
     public static void Greet() {
-        String start = "Hello, I'm Duke.Duke\n"
+        String start = "Hello, I'm Duke.\n"
                 + "What can I do for you?\n";
         System.out.println(line + "\n" + start + line + "\n");
     }
 
+    /**
+     * Prints a message with the number of task inside the task list of Duke.
+     */
     public static void PrintTaskCount() {
         System.out.println("Now you have " + TaskList.size() + " tasks in the list.");
     }
 
+    /**
+     * Prints a message that a Task that has been added.
+     * @param newTask
+     */
     public static void PrintTaskAdded(Task newTask) {
         System.out.println("Got it. I've added this task:\n"
                 + newTask.getTaskInfo());
