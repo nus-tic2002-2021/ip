50b6afeaa620dd3f4296525abb0c8fee5395ed83
ksw95 <kshaowei95@gmail.com>
2021-11-07 01:07:49 +0800

    Added comments to the class and methods in DukeException.java

--- src/main/java/Duke/DukeException.java
+++ src/main/java/Duke/DukeException.java
@@ -1,16 +1,30 @@
 package Duke;
 
+/**
+ * An exception class that represents the errors that are thrown when using duke.
+ */
 public class DukeException extends Exception{
     protected String ErrMsg;
 
+    /**
+     * DukeException constructor that creates a DukeException
+     * with an empty error message.
+     */
     public DukeException() {
         this.ErrMsg = "";
     }
 
+    /**
+     * DukeException constructor takes in a custom error message.
+     * @param invalid
+     */
     public DukeException(String invalid) {
         this.ErrMsg = invalid;
     }
 
+    /**
+     * Prints the custom error message of a DukeException if any.
+     */
     public void printErrMsg() {
         System.out.println(ErrMsg);
     }
