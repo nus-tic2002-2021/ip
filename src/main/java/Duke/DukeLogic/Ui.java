304462ff62a2c253509fec5f4ce84a06c2adb4c9
ksw95 <kshaowei95@gmail.com>
2021-11-11 01:07:28 +0800

    Improved the coding standard of Ui.java

--- src/main/java/Duke/DukeLogic/Ui.java
+++ src/main/java/Duke/DukeLogic/Ui.java
@@ -1,39 +1,38 @@
-package Duke.Ui;
+package Duke.DukeLogic;
 
 import Duke.Models.Task;
-import Duke.TaskList;
 
 /**
  * Ui class holds Ui display methods that interact with the user.
  */
 public class Ui {
-    public static String line = "____________________________________________________________";
+    public final static String LINE = "____________________________________________________________";
 
     /**
      * Prints the opening message of Duke.
      */
-    public static void StartDuke() {
+    public static void startDuke() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(line + "\nWelcome to\n" + logo + " chatbot!");
+        System.out.println(LINE + "\nWelcome to\n" + logo + " chatbot!");
     }
 
     /**
      * Prints a greeting from Duke.
      */
-    public static void Greet() {
+    public static void greet() {
         String start = "Hello, I'm Duke.\n"
                 + "What can I do for you?\n";
-        System.out.println(line + "\n" + start + line + "\n");
+        System.out.println(LINE + "\n" + start + LINE + "\n");
     }
 
     /**
      * Prints a message with the number of task inside the task list of Duke.
      */
-    public static void PrintTaskCount() {
+    public static void printTaskCount() {
         System.out.println("Now you have " + TaskList.size() + " tasks in the list.");
     }
 
@@ -41,8 +40,8 @@ public class Ui {
      * Prints a message that a Task that has been added.
      * @param newTask
      */
-    public static void PrintTaskAdded(Task newTask) {
-        System.out.println(line);
+    public static void printTaskAdded(Task newTask) {
+        System.out.println(LINE);
         System.out.println("Got it. I've added this task:\n"
                 + newTask.getTaskInfo());
     }
@@ -51,6 +50,6 @@ public class Ui {
         System.out.println("What is the priority level of this task?" +
                 "\n(enter 1 - LOW priority, 2 - MEDIUM priority, 3 - HIGH priority." +
                 "\nOther inputs will assume task has NIL priority level.)");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
