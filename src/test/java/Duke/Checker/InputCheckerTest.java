560e0ca75708f024c0683acf6b68db79521d6507
ksw95 <kshaowei95@gmail.com>
2021-11-11 02:53:35 +0800

    Edited method calls in InputCheckerTest class to reflect refactoring and edits done to InputChecker class

--- src/test/java/Duke/Checker/InputCheckerTest.java
+++ src/test/java/Duke/Checker/InputCheckerTest.java
@@ -10,33 +10,33 @@ public class InputCheckerTest {
 
     @Test
     public void checkValidTodoTest() throws DukeException {
-        assertEquals(true, checkValidTodo("todo task 1"));
-        assertThrows(DukeException.class, () -> checkValidTodo("tod"));
-        assertThrows(DukeException.class, () -> checkValidTodo("todo    "));
+        assertEquals(true, isValidTodoInput("todo task 1"));
+        assertThrows(DukeException.class, () -> isValidTodoInput("tod"));
+        assertThrows(DukeException.class, () -> isValidTodoInput("todo    "));
     }
 
     @Test
     public void checkValidDeadlineTest() throws DukeException {
-        assertEquals(true, checkValidDeadline("deadline homework /by today"));
-        assertThrows(DukeException.class, () -> checkValidDeadline("deadli"));
-        assertThrows(DukeException.class, () -> checkValidDeadline("deadline return book /by"));
-        assertThrows(DukeException.class, () -> checkValidDeadline("deadline /by tomorrow"));
+        assertEquals(true, isValidDeadlineInput("deadline homework /by today"));
+        assertThrows(DukeException.class, () -> isValidDeadlineInput("deadli"));
+        assertThrows(DukeException.class, () -> isValidDeadlineInput("deadline return book /by"));
+        assertThrows(DukeException.class, () -> isValidDeadlineInput("deadline /by tomorrow"));
 
     }
 
     @Test
     public void checkValidEventTest() throws DukeException {
-        assertEquals(true, checkValidEvent("event assembly /at school"));
-        assertThrows(DukeException.class, () -> checkValidEvent("eve"));
-        assertThrows(DukeException.class, () -> checkValidEvent("event birthday /at"));
-        assertThrows(DukeException.class, () -> checkValidEvent("event /at home"));
+        assertEquals(true, isValidEventInput("event assembly /at school"));
+        assertThrows(DukeException.class, () -> isValidEventInput("eve"));
+        assertThrows(DukeException.class, () -> isValidEventInput("event birthday /at"));
+        assertThrows(DukeException.class, () -> isValidEventInput("event /at home"));
     }
 
     @Test
     public void checkValidFindTest() throws DukeException {
-        assertEquals(true, checkValidFind(new String[]{"find","task"}));
-        assertThrows(DukeException.class, () -> checkValidFind(new String[]{"find"}));
-        assertThrows(DukeException.class, () -> checkValidFind(new String[]{}));
+        assertEquals(true, isValidFindInput(new String[]{"find","task"}));
+        assertThrows(DukeException.class, () -> isValidFindInput(new String[]{"find"}));
+        assertThrows(DukeException.class, () -> isValidFindInput(new String[]{}));
 
     }
 }
