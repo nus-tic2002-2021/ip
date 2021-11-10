df8d85f1c0cb428a10788cee6eb400e9ad35abba
ksw95 <kshaowei95@gmail.com>
2021-11-11 01:12:29 +0800

    Updated FileLineCheckerTest.java to reflect refactored methods from other classes.

--- src/test/java/Duke/Checker/FileLineCheckerTest.java
+++ src/test/java/Duke/Checker/FileLineCheckerTest.java
@@ -8,23 +8,23 @@ public class FileLineCheckerTest {
 
     @Test
     public void checkTodoLineTest() {
-        assertEquals(true,CheckTodoLine(new String[]{"T", "1", "Homework", "MEDIUM"}));
-        assertEquals(false, CheckTodoLine(new String[]{"", "", ""}));
+        assertEquals(true, checkTodoLine(new String[]{"T", "1", "Homework", "MEDIUM"}));
+        assertEquals(false, checkTodoLine(new String[]{"", "", ""}));
     }
 
     @Test
     public void checkDeadlineLineTest() {
         assertEquals(true,
-                CheckDeadlineLine(new String[]{"D", "0", "Submit Project", "Sunday", "HIGH"}));
+                checkDeadlineLine(new String[]{"D", "0", "Submit Project", "Sunday", "HIGH"}));
         assertEquals(false,
-                CheckDeadlineLine(new String[]{"D", "3", "Do task", "Today", "LOW"}));
+                checkDeadlineLine(new String[]{"D", "3", "Do task", "Today", "LOW"}));
     }
 
     @Test
     public void checkEventLineTest() {
         assertEquals(true,
-                CheckEventLine(new String[]{"E", "1", "Party", "Friend's house", "LOW"}));
+                checkEventLine(new String[]{"E", "1", "Party", "Friend's house", "LOW"}));
         assertEquals(false,
-                CheckEventLine(new String[]{"E", "0", "", "Nowhere", "NIL"}));
+                checkEventLine(new String[]{"E", "0", "", "Nowhere", "NIL"}));
     }
 }
