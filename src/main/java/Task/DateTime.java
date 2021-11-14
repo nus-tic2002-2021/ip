4a2172d44d057cd2c3b6a46e9b9294144df42c3f
iamMONSTER <jochuing83@gmail.com>
2021-11-14 12:34:15 +0800

    Level-8: Dates and Times

--- src/main/java/Task/DateTime.java
+++ src/main/java/Task/DateTime.java
@@ -1,8 +1,12 @@
 package Task;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTime {
     protected String condition;
-    protected String time;
+    //protected String time;
+    protected Date date;
 
     public DateTime() {
 
@@ -10,15 +14,23 @@ public class DateTime {
 
     public DateTime(String condition, String time) {
         this.condition = condition;
-        this.time = time;
+        ///this.time = time;
+        try {
+            this.date =new SimpleDateFormat("dd/MM/yyyy HHmm").parse(time);
+        } catch (Exception e) {
+
+        }
     }
 
     public String getCondition() {
         return condition;
     }
 
-    public String getTime() {
-        return time;
-    }
+//    public String getTime() {
+//        return time;
+//    }
 
+    public String getTime() {
+        return date.toString();
+    }
 }
